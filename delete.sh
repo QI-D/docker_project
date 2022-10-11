25bba210dabe9b9ad0fced449ad2431e4594e568
QI-D <jonsondang@outlook.com>
2022-10-10 18:55:24 -0700

    modify automating files for adding analysis service
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- delete.sh
+++ delete.sh
@@ -2,4 +2,4 @@ docker-compose down
 docker image rm qidang/project1:frontend
 docker image rm qidang/project1:backend
 docker image rm qidang/project1:storage
-
+docker image rm qidang/project1:analysis
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+docker build -t qidang/project1:analysis analysis/
 docker build -t qidang/project1:frontend frontend/
 docker build -t qidang/project1:backend backend/
 docker build -t qidang/project1:storage storage/
