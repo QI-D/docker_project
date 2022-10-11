207d363245aa2d12819bf059c046620a5f3ab0ea
QI-D <jonsondang@outlook.com>
2022-10-10 17:30:28 -0700

    Create sql file for user, db, table creation
    Signed-off-by: QI-D <jonsondang@outlook.com>

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE orders;
+
+USE orders;
+
+CREATE USER 'mysql_user'@'%' IDENTIFIED BY 'SecuRe_pwd1';
+GRANT ALL PRIVILEGES ON orders.* TO 'mysql_user'@'%';
+
+CREATE TABLE IF NOT EXISTS expense
+          (id INT NOT NULL AUTO_INCREMENT,
+           order_id VARCHAR(36) NOT NULL,
+           item_id VARCHAR(36) NOT NULL,
+           item_name VARCHAR(250) NOT NULL,
+           quantity INTEGER NOT NULL,
+           price DECIMAL NOT NULL,
+           timestamp VARCHAR(100) NOT NULL,
+           date_created VARCHAR(100) NOT NULL,
+           trace_id VARCHAR(36) NOT NULL,
+           PRIMARY KEY (id));
